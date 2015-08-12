6b39ba682c26407ef4dcde44b0fddb2ccc1414e4
Ada Lovelace <marie@wolfssl.com>
2015-08-12 09:56:17 -0600

    Adding examples

---
+++ stunnel/example/PSK/wolfExamples/client-psk.c
@@ -0,0 +1,166 @@
+
+/* client-psk.c
+ *
+ * Copyright (C) 2006-2015 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL. (formerly known as CyaSSL)
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ **/
+
+#include    <stdio.h>
+#include    <stdlib.h>
+#include    <string.h>
+#include    <errno.h>
+#include    <arpa/inet.h>
+#include    <signal.h>
+#include    <unistd.h>
+#include    <wolfssl/ssl.h>  /* must include this to use wolfSSL security */
+
+#define     MAXLINE 256      /* max text line length */
+#define     SERV_PORT 11112  /* default port*/
+
+/*
+ *psk client set up.
+ */
+
+
+static inline unsigned int my_Psk_Client_Cb(WOLFSSL* ssl, const char* hint,
+        char* identity, unsigned int id_max_len, unsigned char* key, 
+        unsigned int key_max_len)
+{
+    (void)ssl;
+    (void)hint;
+    (void)key_max_len;
+    
+    char* keyToBe = "1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d";
+    int   keyLen  = strlen(keyToBe);
+
+    /* identity is OpenSSL testing default for openssl s_client, keep same*/
+    strncpy(identity, "Client_identity", id_max_len);
+    strncpy((char*)key, keyToBe, keyLen);
+
+    return keyLen;
+}
+
+/*
+ * this function will send the inputted string to the server and then 
+ * recieve the string from the server outputing it to the termial
+ */ 
+int SendReceive(WOLFSSL* ssl)
+{
+    char sendline[MAXLINE]="Happy Tuesday (It is really Monday)"; /* string to send to the server */
+    char recvline[MAXLINE]; /* string received from the server */
+        
+	/* write string to the server */
+    int temp = wolfSSL_write(ssl, sendline, MAXLINE);
+	if ( temp!= sizeof(sendline)) {
+		printf("Write Error to Server\t%d\n", temp);
+		return 1;
+    }
+        
+	/* flags if the Server stopped before the client could end */
+    if (wolfSSL_read(ssl, recvline, MAXLINE) < 0 ) {
+    	printf("Client: Server Terminated Prematurely!\n");
+        return 1;
+    }
+
+    /* show message from the server */
+	printf("Server Message: %s\n", recvline);
+        	
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+    wolfSSL_Debugging_ON();
+    int ret, sockfd;
+    WOLFSSL* ssl;
+    WOLFSSL_CTX* ctx;
+    struct sockaddr_in servaddr;;
+
+    /* must include an ip address of this will flag */
+    if (argc != 2) {
+        printf("Usage: tcpClient <IPaddress>\n");
+        return 1;
+    }
+    
+    wolfSSL_Init();  /* initialize wolfSSL */
+    
+    /* create and initialize WOLFSSL_CTX structure */
+    if ((ctx = wolfSSL_CTX_new(wolfTLSv1_2_client_method())) == NULL) {
+        fprintf(stderr, "SSL_CTX_new error.\n");
+        return 1;
+    }
+                
+    /* create a stream socket using tcp,internet protocal IPv4,
+     * full-duplex stream */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    /* places n zero-valued bytes in the address servaddr */
+    memset(&servaddr, 0, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+
+    /* converts IPv4 addresses from text to binary form */
+    ret = inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+
+    if (ret != 1) {
+        printf("inet_pton error\n");    
+		return 1;
+    }
+    
+    /* set up pre shared keys */
+    wolfSSL_CTX_set_psk_client_callback(ctx, my_Psk_Client_Cb);
+	
+    /* attempts to make a connection on a socket */
+    ret = connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+    
+    if (ret != 0) {
+        printf("Connection Error\n");
+        return 1;
+    }
+    
+    /* creat wolfssl object after each tcp connct */
+    if ( (ssl = wolfSSL_new(ctx)) == NULL) {
+        fprintf(stderr, "wolfSSL_new error.\n");
+        return 1;
+    }
+	
+    /* associate the file descriptor with the session */
+    ret = wolfSSL_set_fd(ssl, sockfd);
+	
+    if (ret != SSL_SUCCESS){
+        return 1;
+    }
+	
+    /* takes inputting string and outputs it to the server */
+	ret = SendReceive(ssl);
+	if(ret != 0){
+		return 1;
+	}
+
+    /* cleanup */
+    wolfSSL_free(ssl);
+
+    /* when completely done using SSL/TLS, free the 
+     * wolfssl_ctx object */
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
+
+    /* exit client */
+    return ret;
+}
---
+++ stunnel/example/PSK/wolfExamples/server-psk.c
@@ -0,0 +1,183 @@
+/* server-psk.c
+ * A server ecample using a TCP connection with PSK security. 
+ *  
+ * Copyright (C) 2006-2015 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL. (formerly known as CyaSSL)
+ *
+ * wolfSSL is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfSSL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <wolfssl/ssl.h>     /* include wolfSSL security */
+#include <wolfssl/options.h> /* included for options sync */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#define MAXLINE     4096
+#define LISTENQ     1024
+#define SERV_PORT   11112
+
+/* 
+ * Handles response to client.
+ */
+int respond(WOLFSSL* ssl)
+{
+    int  n;              /* length of string read */
+    char buf[MAXLINE];   /* string read from client */
+    char response[] = "I hear ya for shizzle";
+    memset(buf, 0, MAXLINE);
+    n = wolfSSL_read(ssl, buf, MAXLINE);
+    if (n > 0) {
+        printf("%s\n", buf);
+        if (wolfSSL_write(ssl, response, strlen(response)) > strlen(response)) {
+            printf("Fatal error : respond: write error\n");
+            return 1;
+        }
+    }
+    if (n < 0) {
+        printf("Fatal error :espond: read error\n");
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+ * Identify which psk key to use.
+ */
+
+static unsigned int my_psk_server_cb(WOLFSSL* ssl, const char* identity, unsigned char* key,
+                              unsigned int key_max_len)
+{
+    (void)ssl;
+    (void)key_max_len;
+
+    if (strncmp(identity, "Client_identity", 15) != 0)
+        return 0;
+
+    
+    char* keyToBe = "1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d1a2b3c4d";
+    int   keyLen  = strlen(keyToBe);
+    strncpy((char*)key, keyToBe, keyLen);
+
+    return keyLen;
+}
+
+int main()
+{
+    wolfSSL_Debugging_ON();
+    int                 listenfd, connfd;
+    int                 opt;
+    struct sockaddr_in  cliAddr, servAddr;
+    char                buff[MAXLINE];
+    socklen_t           cliLen;
+    WOLFSSL_CTX*         ctx;
+
+    wolfSSL_Init();
+    
+    /* create ctx and configure certificates */
+    if ((ctx = wolfSSL_CTX_new(wolfSSLv23_server_method())) == NULL) {
+        printf("Fatal error : wolfSSL_CTX_new error\n");
+        return 1;
+    }
+   
+    /* use psk suite for security */ 
+    wolfSSL_CTX_set_psk_server_callback(ctx, my_psk_server_cb);
+    wolfSSL_CTX_use_psk_identity_hint(ctx, "wolfssl server");
+    if (wolfSSL_CTX_set_cipher_list(ctx, "PSK-AES128-CBC-SHA")
+                                   != SSL_SUCCESS) {
+        printf("Fatal error : server can't set cipher list\n");
+        return 1;
+    }
+
+
+    /* set up server address and port */
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family      = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port        = htons(SERV_PORT);
+
+    /* find a socket */ 
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
+        printf("Fatal error : socket error\n");
+        return 1;
+    }
+
+    /* bind to a socket */
+    opt = 1;
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt,
+               sizeof(int)) != 0) {
+        printf("Fatal error : setsockopt error\n");
+        return 1;
+    }
+    if (bind(listenfd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+        printf("Fatal error : bind error\n");
+        return 1;
+    }
+    
+    /* listen to the socket */   
+    if (listen(listenfd, LISTENQ) < 0) {
+        printf("Fatal error : listen error\n");
+        return 1;
+    }
+    
+    /* main loop for accepting and responding to clients */
+    for ( ; ; ) {
+        WOLFSSL* ssl;
+        
+        cliLen = sizeof(cliAddr);
+        connfd = accept(listenfd, (struct sockaddr *) &cliAddr, &cliLen);
+        if (connfd < 0) {
+            printf("Fatal error : accept error\n");
+            return 1;
+        }
+        else {
+            printf("Connection from %s, port %d\n",
+                   inet_ntop(AF_INET, &cliAddr.sin_addr, buff, sizeof(buff)),
+                   ntohs(cliAddr.sin_port));
+            
+            /* create WOLFSSL object and respond */
+            if ((ssl = wolfSSL_new(ctx)) == NULL) {
+                printf("Fatal error : wolfSSL_new error\n");
+                return 1;
+            }
+            wolfSSL_set_fd(ssl, connfd);
+            if (respond(ssl) != 0)
+                return 1;
+            
+            /* closes the connections after responding */
+            wolfSSL_shutdown(ssl);
+            wolfSSL_free(ssl);
+            
+            if (close(connfd) == -1) {
+                printf("Fatal error : close error\n");
+                return 1;
+            }
+        }
+    }
+    /* free up memory used by wolfSSL */
+    wolfSSL_CTX_free(ctx);
+    wolfSSL_Cleanup();
+
+    return 0;
+}
+
