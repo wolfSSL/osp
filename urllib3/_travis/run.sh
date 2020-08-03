5b8f54c71de95a4f324f0624307c6fd623bae81a
Tesfa Mael <tesfa@wolfssl.com>
2020-08-03 14:07:34 -0700

    urllib3 tag 1.25.8

--- urllib3/_travis/run.sh
+++ urllib3/_travis/run.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-set -e
-set -x
+set -exo pipefail
 
-if [[ "$(uname -s)" == "Darwin" && "$TOXENV" == "py27" ]]; then
+if [[ "$(uname -s)" == "Darwin" && "$NOX_SESSION" == "tests-2.7" ]]; then
     export PATH="/Library/Frameworks/Python.framework/Versions/2.7/bin":$PATH
 fi
 
-tox
+if [ -n "${NOX_SESSION}" ]; then
+    if [[ "$(uname -s)" == 'Darwin' ]]; then
+        # Explicitly use python3 on macOS as `nox` is not in the PATH
+        python3 -m nox -s "${NOX_SESSION}"
+    else
+        nox -s "${NOX_SESSION}"
+    fi
+else
+    downstream_script="${TRAVIS_BUILD_DIR}/_travis/downstream/${DOWNSTREAM}.sh"
+    if [ ! -x "$downstream_script" ]; then
+        exit 1
+    fi
+    $downstream_script install
+    python -m pip install .
+    $downstream_script run
+fi
